066ec3d57bb3f5b12b195d6252bcb9a716718e99
AishwaryaPai20 <aishwaryampai200203@gmail.com>
2022-07-29 20:41:11 +0530

    Connected Firebase to Flutter

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (ctx, index) => Container(
-        padding: const EdgeInsets.all(8),
-        child: const Text('This works'),
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (ctx, index) => Container(
+          padding: const EdgeInsets.all(8),
+          child: const Text('This works'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Firestore.instance
+              .collection('chats/3hBv43xAVZwvLh42GvVr/messages')
+              .snapshots()
+              .listen((data) {
+            data.documents.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
       ),
     );
   }
