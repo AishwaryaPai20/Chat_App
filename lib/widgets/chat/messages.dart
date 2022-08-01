34dd1dcc813e554677211e267788da936c72b71a
AishwaryaPai20 <aishwaryampai200203@gmail.com>
2022-08-02 01:21:42 +0530

    Image Picker Added

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import './message_bubble.dart';
+import 'message_bubble.dart';
 
 class Messages extends StatelessWidget {
   @override
@@ -37,9 +37,7 @@ class Messages extends StatelessWidget {
                   chatDocs[index]['text'],
                   chatDocs[index]['username'],
                   chatDocs[index]['userId'] == futureSnapshot.data.uid,
-                  key: ValueKey(
-                    chatDocs[index].documentID,
-                  ),
+                  key: ValueKey(chatDocs[index].documentID),
                 ),
               );
             });
